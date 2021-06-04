f9342cbacb5eaba3400260b169ac81599b68e578
mastro-elfo <francesco.209@gmail.com>
2021-06-04 23:26:22 +0200

    Setup deploy action

--- scripts/init.js
+++ scripts/init.js
@@ -1,3 +1,5 @@
 const { version } = require("../package.json");
 const { writeFileSync } = require("fs");
+
+// Initialize file src/version.json
 writeFileSync("src/version.json", JSON.stringify({ version }));
--- scripts/posttest.js
+++
@@ -1,8 +0,0 @@
-const { LocalStorageBackendOptions } = require("../src/i18n.json");
-if (LocalStorageBackendOptions.expirationTime !== undefined) {
-  console.error(
-    "Don't overwrite default expirationTime in production",
-    LocalStorageBackendOptions.expirationTime
-  );
-  process.exit(1);
-}
