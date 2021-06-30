89270401050e3ebb9aeadeebe395d128c33c021b
mastro-elfo <francesco.209@gmail.com>
2021-06-30 18:48:40 +0200

    Add due value to PerformanceListItem

--- src/pages/performance/PerformanceListItem.jsx
+++ src/pages/performance/PerformanceListItem.jsx
@@ -8,6 +8,7 @@ import {
   Typography,
 } from "@material-ui/core";
 
+import { useSettings } from "../settings/context";
 import { delta2hms, timeDiff } from "../tracker/utils";
 
 import CheckIcon from "@material-ui/icons/CheckCircle";
@@ -18,14 +19,25 @@ export default function PerformanceListItem({
   onToggle,
   performance,
 }) {
+  const [settings] = useSettings();
   const {
     client: { name, surname },
+    discount,
     end,
     note,
     pauses,
     payed,
+    payments,
+    price,
     start,
   } = performance;
+
+  const length = timeDiff(start, end, pauses);
+  const cost = (length / 1000 / 3600) * price;
+  const total = cost - discount;
+  const balance = payments.reduce((acc, { value }) => acc + value, 0);
+  const due = total - balance;
+
   return (
     <ListItem button onClick={onClick}>
       <ListItemIcon>
@@ -34,21 +46,27 @@ export default function PerformanceListItem({
       <ListItemText
         primary={
           <Grid container>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={3}>
               <Typography variant="h6">
                 {name} {surname}
               </Typography>
             </Grid>
-            <Grid item xs={6} sm={4}>
+            <Grid item xs={4} sm={3}>
               <ListItemText
                 primary={new Date(start).toLocaleString()}
                 secondary="Data"
               />
             </Grid>
-            <Grid item xs={6} sm={4}>
+            <Grid item xs={4} sm={3}>
               <ListItemText
                 primary={delta2hms(timeDiff(start, end, pauses))}
                 secondary="Durata"
+              />
+            </Grid>
+            <Grid item xs={4} sm={3}>
+              <ListItemText
+                primary={`${parseFloat(due).toFixed(2)} ${settings.currency}`}
+                secondary="Dovuto"
               />
             </Grid>
           </Grid>
