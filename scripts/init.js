ab0780b38963037ed456311dc77842489d8f276a
mastro-elfo <francesco.209@gmail.com>
2021-07-08 11:46:46 +0200

    Check current branch in development mode

--- scripts/init.js
+++ scripts/init.js
@@ -1,5 +1,16 @@
 const { version } = require("../package.json");
 const { writeFileSync } = require("fs");
+const { execSync } = require("child_process");
+
+// console.log(process.argv);
 
 // Initialize file src/version.json
 writeFileSync("src/version.json", JSON.stringify({ version }));
+
+// Get current branch name
+const branch = execSync("git branch --show-current", {
+  encoding: "utf-8",
+}).trim();
+if (process.argv.includes("--dev") && branch !== "develop") {
+  console.warn("WARN Current branch:", branch);
+}
